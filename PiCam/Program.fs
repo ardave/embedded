e62d5e9b05eb657c59faf36f08e8d2a39bd58053
Dave <ardave@gmail.com>
2022-07-31 11:11:41 -0600

    Added unosquare packages but doesn't work yet

--- PiCam/Program.fs
+++ PiCam/Program.fs
@@ -1,18 +1,25 @@
 ﻿open System
 open System.Threading
+open Unosquare.RaspberryIO
+open Unosquare.WiringPi
 
 let takePicture() = ()
 let nextPictureIn() = TimeSpan.FromMinutes 15.
 let uploadPicture() = ()
 
 let doIt tp np up =
+    let pictureBytes = Pi.Camera.CaptureImageJpeg(640, 480)
+    printfn "Picture is %i bytes" pictureBytes.Length
     ()
 
 let rec loop() =
+    doIt () () () 
+
     let timeSpan = nextPictureIn()
     printfn "Sleeping for %O" timeSpan
     Thread.Sleep timeSpan
     loop()
 
+Pi.Init<BootstrapWiringPi>()
 loop()
 printfn "Done."
