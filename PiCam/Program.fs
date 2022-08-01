917bc2bf7b0a7d9313f67849a42a4c6ddbc3e31d
David Falkner <ardave@gmail.com>
2022-08-01 00:00:34 +0000

    Filling in pi camera capture code.
    Maybe keeping it testable?

--- PiCam/Program.fs
+++ PiCam/Program.fs
@@ -1,37 +1,46 @@
 ﻿open System
+open System.IO
 open System.Threading
 open MMALSharp
 open MMALSharp.Common
 open MMALSharp.Common.Utility
 open MMALSharp.Handlers
 
-let takePicture() = ()
 let nextPictureIn() = TimeSpan.FromSeconds 15.
-let uploadPicture() = ()
+let uploadPicture (bytes: byte[]) = ()
+
+let microsecondsPerSecond = 1000000.
 
 MMALCameraConfig.StillResolution <- new Resolution(4056, 3040)
-//MMALCameraConfig.Framerate <- new MMAL_RATIONAL_T(20, 1) // Set to 20fps. Default is 30fps.
-MMALCameraConfig.ShutterSpeed <- 2000000 // Set to 2s exposure time. Default is 0 (auto).
-MMALCameraConfig.ISO <- 400 // Set ISO to 400. Default is 0 (auto).
+MMALCameraConfig.ShutterSpeed <- int (1. / 60. * microsecondsPerSecond)
+MMALCameraConfig.ISO <- 100
 
 let cam: MMALCamera = MMALCamera.Instance;
 
-let doIt tp np up =
-    printfn "Gonna take a picture."
-    use imgCaptureHandler = new ImageStreamCaptureHandler("/home/dave/Pictures", "jpg")
+let streamToBytes (inputStream: Stream) : byte[] =
+    use memoryStream = new MemoryStream()
+    inputStream.CopyTo(memoryStream)
+    memoryStream.ToArray()
 
-    cam.TakePicture(imgCaptureHandler, MMALEncoding.JPEG, MMALEncoding.I420)
+let takePicture() : byte[] =
+    use msch = new MemoryStreamCaptureHandler()
+    
+    cam.TakePicture(msch, MMALEncoding.JPEG, MMALEncoding.I420)
     |> Async.AwaitTask
     |> Async.RunSynchronously
 
-    printfn "Took a picture."    
+    msch.CurrentStream
+    |> streamToBytes
 
-let rec loop() =
-    doIt () () ()
-    let timeSpan = nextPictureIn()
-    printfn "Sleeping for %O" timeSpan
-    Thread.Sleep timeSpan
-    loop()
 
-loop()
+let rec loop tp np up =
+    let nextPictureIn: TimeSpan = np()
+    Thread.Sleep nextPictureIn
+
+    tp() |> up   
+        
+    loop tp np up
+
+loop takePicture nextPictureIn uploadPicture
+
 printfn "Done."
