fe9febe2792e9ee6133cefe2158596c305da8525
David Falkner <ardave@gmail.com>
2022-08-07 18:04:54 +0000

    gonna try removing NextPictureModel

--- PiCam/PictureTaker/AzureFunctionInteractor.fs
+++ PiCam/PictureTaker/AzureFunctionInteractor.fs
@@ -3,6 +3,7 @@
     open System
     open System.Collections.Generic
     open System.Net.Http
+    open Newtonsoft.Json
     open PiCamCommon    
 
     let private client = new HttpClient()
@@ -13,12 +14,19 @@
     queue.Enqueue(TimeSpan.FromMinutes 15.)
     queue.Enqueue(TimeSpan.FromMinutes 15.)
     
-    let nextPictureIn() = 
+    let nextPictureIn(): NextOperation = 
         if queue.Count = 0 then
             NextOperation.ExitLoop
         else
             NextOperation.TakeNextPictureIn (queue.Dequeue())
 
-    let nextPictureInHttp() =
+    let nextPictureInHttp(): NextOperation =
         let response = client.GetAsync("uri") |> Async.AwaitTask |> Async.RunSynchronously
-        ()
+        let content: string = response.Content.ReadAsStringAsync() |> Async.AwaitTask |> Async.RunSynchronously
+
+        if response.IsSuccessStatusCode then
+            failwith "not implemented"
+            //content |> JsonConvert.DeserializeObject<NextPictureModel> |> NextOperation.TakeNextPictureIn
+        else
+            failwith $"Unsuccessful status code {response.StatusCode} getting next picture TimeSpan, with content: '{content}'."           
+    
